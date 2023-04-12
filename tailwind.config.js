fbc18b361a1e9b6ccd0480ef9a5d61b44f380e11
deeokafor <okafor.udahdivine@gmail.com>
2023-04-12 07:46:58 +0100

    built rating card and made it responsive

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html, js}'],
+  theme: {
+    extend: {
+      colors: {
+        'ir-orange': 'hsl(25, 97%, 53%)',
+        'ir-light-grey': 'hsl(217, 12%, 63%)',
+        'ir-medium-grey': 'hsl(216, 12%, 54%)',
+        'ir-dark-blue': 'hsl(213, 19%, 18%)',
+        'ir-very-dark-blue': 'hsl(216, 12%, 8%)',
+      }
+    },
+  },
+  plugins: [],
+  prefix: 'tw-'
+}
