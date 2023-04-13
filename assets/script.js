e58bc4f50f3d91697eb7594822a906a6fb59bc3a
deeokafor <okafor.udahdivine@gmail.com>
2023-04-13 14:05:30 +0100

    add user capability to select a number for rating

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-console.log(`live!!`)
+// initialized all variables to be worked with
+let rating = document.querySelectorAll('.rating');
+let user_selected_rating = document.querySelector('#user_selected_rating').textContent;
+
+// using a for loop mapped to html elements to get user selection
+function get_user_selected_rating() {
+    for (let i = 0; i < rating.length; i++) {
+        rating[i].addEventListener('click', function () {
+            // console.log('you clicked' + ' ' + rating[i].innerHTML);
+            // user_selected_rating = rating.innerHtml;
+        })
+    }
+}
+get_user_selected_rating();
+
+// functions for adding and/or removing classNames to html elements
+function add_class(element, class_name) {
+    element.classList.add(element, class_name);
+}
+
+function remove_class(element, class_name) {
+    element.classList.remove(element, class_name);
+}
+
