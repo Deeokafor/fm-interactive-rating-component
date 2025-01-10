a5b85d4c11003bc6315dc64bf9a0ab71b32408c4
Deeokafor <okafor.udahdivine@gmail.com>
2025-01-10 14:49:48 +0100

    converting to a React.js App

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="tw-text-white/50 tw-text-center">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="tw-underline tw-underline-offset-2" rel="noreferrer noopener">Frontend Mentor</a>. Coded by <a href="https://github.com/Deeokafor/fr-interactive-rating-component" className="tw-underline tw-underline-offset-2">Okafor-udah Divine </a>.
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // import { useState } from 'react'
 // import favicon from '../src/assets/images/favicon-32x32.png'
-import './App.css'
-import './custom.css'
+import './App.css';
+import './custom.css';
+import Button from '../components/Button.jsx';
+import Footer from '../components/Footer.jsx';
+import RatingList from '../components/RatingList.jsx';
 
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="tw-bg-black">Hello World</h1>
-    </>
+    <div>
+      <RatingList />
+      <Footer />
+      <Button />
+    </div>
   )
 }
 
